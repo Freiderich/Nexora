87a7dabfb4f21c0d2712e54fa6c2c5d5cb903062
Freiderich <freiderichperalta@gmail.com>
2026-05-19 18:26:24 +0800

    Splitting the css into separate files

--- users/forms.py
+++ users/forms.py
@@ -6,11 +6,24 @@ from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email", "").strip()
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data.get("email", "")
+        if commit:
+            user.save()
+        return user
 
 
 class ProfileForm(forms.ModelForm):
